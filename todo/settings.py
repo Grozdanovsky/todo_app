acf65b9f8d86e61a17254fadb29f55faadffb29f
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-03-05 19:35:33 +0100

    Added register and login for users with jwt tokens

--- todo/settings.py
+++ todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rest_framework',
+    'djoser',
     'list',
     'core',
 ]
@@ -137,5 +139,26 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+       
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
+}
+
+
+
+DJOSER = {
+    'SERIALIZERS' : {
+        'user_create' : 'core.serializers.UserCreateSerializer'
+    }
+}
+
 
 AUTH_USER_MODEL = 'core.User'
+
