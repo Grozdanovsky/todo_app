8196056868ba870d803b0bbfd8e3f35cf467baf0
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-03-07 15:46:58 +0100

    Added some permissions

--- list/views.py
+++ list/views.py
@@ -3,7 +3,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.views import APIView
 from rest_framework.mixins import *
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+from list.permissions import IsAdminOrReadOnly
 from .models import Customer,TodoList
+from rest_framework.decorators import action
 from .serializers import CustomerSerializer,  TodoSerializer
 # Create your views here.
 
@@ -13,6 +17,7 @@ class TodoListViewSet(ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
 
+    permission_classes = [IsAdminOrReadOnly]
 
 
 
@@ -30,6 +35,24 @@ class CustomerTodo(APIView):
         return Response(status = status.HTTP_204_NO_CONTENT)
         
 
-class CustomerViewSet(CreateModelMixin,RetrieveModelMixin,UpdateModelMixin, GenericViewSet):
+class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return[AllowAny()]
+        return [IsAuthenticated()]
+
+    @action(detail=False, methods = ['GET','PUT'], permission_classes = [IsAuthenticated]) # this action is avilable on list view
+    def me(self,request):
+        (customer,created) = Customer.objects.get_or_create(user_id = request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
